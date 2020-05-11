daf091530f74eef1ee6ed863d4651a26dcc632fa
Bartek Walczak <50672367+bl4keee@users.noreply.github.com>
2020-05-12 00:19:47 +0200

    Initial Commit

---
+++ ManyToMany_Mapping/src/com/luv2code/hibernate/demo/CreateCourseAndStudentsDemo.java
@@ -0,0 +1,61 @@
+package com.luv2code.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.luv2code.hibernate.demo.entity.Course;
+import com.luv2code.hibernate.demo.entity.Instructor;
+import com.luv2code.hibernate.demo.entity.InstructorDetail;
+import com.luv2code.hibernate.demo.entity.Review;
+import com.luv2code.hibernate.demo.entity.Student;
+
+public class CreateCourseAndStudentsDemo {
+
+	public static void main(String[] args) {
+
+		SessionFactory factory = new Configuration()
+								.configure("hibernate.cfg.xml")
+								.addAnnotatedClass(Instructor.class)
+								.addAnnotatedClass(InstructorDetail.class)
+								.addAnnotatedClass(Course.class)
+								.addAnnotatedClass(Review.class)
+								.addAnnotatedClass(Student.class)
+								.buildSessionFactory();
+
+		Session session = factory.getCurrentSession();
+		
+		try {			
+						
+			session.beginTransaction();
+		
+			Course tempCourse = new Course("Pacman - How To Score One Million Points");
+			
+			System.out.println("\nSaving the course...");
+			session.save(tempCourse);
+			System.out.println("Saved the course: " + tempCourse);
+			
+			Student tempStudent1 = new Student("John", "Doe", "doe@gmail.com");
+			Student tempStudent2 = new Student("Mary", "Public", "public@gmail.com");
+			
+			tempCourse.addStudent(tempStudent1);
+			tempCourse.addStudent(tempStudent2);
+			
+			System.out.println("\nSaving the students...");
+			session.save(tempStudent1);
+			session.save(tempStudent2);
+			System.out.println("Saved students: " + tempCourse.getStudents());
+			
+			session.getTransaction().commit();
+			
+			System.out.println("Done!");
+		}
+		finally {
+			session.close();
+			factory.close();
+		}
+	}
+
+}
+
+
